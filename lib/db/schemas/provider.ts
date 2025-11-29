11f0c5b09b77c86028bf2dc61c615e2eb3289fac
fabiangzvo <faguzman.97@gmail.com>
2025-11-29 17:12:10 -0500

    add 'enabled'  column to provider table

--- lib/db/schemas/provider.ts
+++ lib/db/schemas/provider.ts
@@ -1,8 +1,9 @@
-import { pgTable, uuid, timestamp, text } from "drizzle-orm/pg-core";
+import { pgTable, uuid, timestamp, text, boolean } from "drizzle-orm/pg-core";
 
 export const provider = pgTable("provider", {
   id: uuid("id").defaultRandom().primaryKey(),
   name: text("name").notNull(),
+  enabled: boolean("enabled").default(true),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at")
     .defaultNow()
