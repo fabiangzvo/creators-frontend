a359b481e3c4dec3c9cb0aa401038daec08d897a
fabiangzvo <faguzman.97@gmail.com>
2025-12-05 19:21:56 -0500

    fix import schemas

---
+++ lib/db/schemas/index.ts
@@ -0,0 +1,7 @@
+export { account } from "./account";
+export { integration } from "./integration";
+export { provider } from "./provider";
+export { session } from "./session";
+export { status } from "./status";
+export { user } from "./user";
+export { verification } from "./verification";
