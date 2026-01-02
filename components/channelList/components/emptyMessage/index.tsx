561765b87c45de894602cb2285084c478c0bed69
fabiangzvo <faguzman.97@gmail.com>
2026-01-02 14:32:47 -0500

    fix empty message color

--- components/channelList/components/emptyMessage/index.tsx
+++ components/channelList/components/emptyMessage/index.tsx
@@ -11,14 +11,14 @@ function EmptyMessage(): JSX.Element {
       <div className="flex gap-4 mb-8">
         <div className="relative border h-24 w-16 flex justify-center items-center rounded-2xl -rotate-20 shadow-lg border-primary-500">
           <div className="bg-primary-50 h-10 w-10 rounded-2xl absolute -z-10" />
-          <TiktokIcon className="fill-primary-500" />
+          <TiktokIcon className="text-primary-500" />
         </div>
         <div className="relative border h-24 w-16 flex justify-center items-center rounded-2xl shadow-lg shadow-primary-500/20 border-primary-500">
-          <InstagramIcon className="[&>rect,&>path]:stroke-primary-500 [&>circle]:fill-primary-500" />
+          <InstagramIcon className="text-primary-500" />
         </div>
         <div className="relative border h-24 w-16 flex justify-center items-center rounded-2xl rotate-20 shadow-lg border-primary-500">
           <div className="bg-primary-50 h-10 w-10 rounded-2xl absolute -z-10" />
-          <FacebookIcon className="[&>path]:fill-primary-500" />
+          <FacebookIcon className="text-primary-500" />
         </div>
       </div>
       <p className="text-foreground/70 text-lg" >Todavía no has conectado ningún canal.</p>
